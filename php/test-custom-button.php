087390b377dfbee0b02124f5731c4f8870125361
Wendy Smoak <wsmoak@gmail.com>
2014-10-10 15:11:31 -0400

    Use jquery to make ajax call when button is clicked

--- php/test-custom-button.php
+++ php/test-custom-button.php
@@ -1,6 +1,8 @@
 <?php require_once('./config.php'); ?>
 
 <script src="https://checkout.stripe.com/checkout.js"></script>
+<script type="text/javascript" src="jquery-1.11.1.js"></script>
+
 
 <button id="customButton">Purchase</button>
 
@@ -11,15 +13,20 @@
     token: function(token) {
       // Use the token to create the charge with a server-side script.
       // You can access the token ID with `token.id`
-      console.log(token)
-      var xhReq = new XMLHttpRequest();
-      var url = "charge-custom-button.php?stripeToken"+token.id;
-      xhReq.open("GET", url, false);
-      xhReq.send(null);
-      var serverResponse = xhReq.responseText;
-      alert(serverResponse);
+      console.log(token);
+      
+      $.ajax({
+        type: "POST",
+        url: "charge-custom-button.php",
+        data: { stripeToken: token.id, stripeEmail: token.email }
+      })
+      .done(function( msg ) {
+          alert( "Server response: " + msg );
+      });  
+      
     }
   });
+  
 
   document.getElementById('customButton').addEventListener('click', function(e) {
     // Open Checkout with further options
