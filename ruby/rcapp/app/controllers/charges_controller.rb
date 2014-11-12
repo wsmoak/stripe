3afc837828e5d50ed6acb7f52a49992eefad8c83
Wendy Smoak <wsmoak@gmail.com>
2014-11-12 18:39:36 -0500

    Add a field for the coupon code and a hidden field for the plan.  Use these when creating a customer and subscribing them to the plan.

--- ruby/rcapp/app/controllers/charges_controller.rb
+++ ruby/rcapp/app/controllers/charges_controller.rb
@@ -8,15 +8,10 @@ class ChargesController < ApplicationController
     @amount = 500
 
     customer = Stripe::Customer.create(
-      :email => 'example@stripe.com',
-      :card  => params[:stripeToken]
-    )
-
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
+      :email => params[:stripeEmsil],
+      :card  => params[:stripeToken],
+      :coupon => params[:coupon_id],
+      :plan => params[:plan_id]
     )
 
   rescue Stripe::CardError => e
